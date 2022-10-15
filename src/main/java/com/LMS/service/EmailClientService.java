9c87a98d516213d51d056a233daae19ba056a757
Shehani9716 <shehani.9716@gmail.com>
2022-10-16 00:37:14 +0530

    Email Client Service update

--- src/main/java/com/LMS/service/EmailClientService.java
+++ src/main/java/com/LMS/service/EmailClientService.java
@@ -1,5 +1,6 @@
 package com.LMS.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -8,24 +9,33 @@ import org.springframework.stereotype.Service;
 
 import javax.mail.internet.MimeMessage;
 import java.io.File;
+import java.util.ArrayList;
 
 @Service
 public class EmailClientService {
 
+    @Autowired
     private JavaMailSender mailSender;
     public void sendSimpleEmail(String toEmail,
                                 String body,
                                 String subject){
         SimpleMailMessage message = new SimpleMailMessage();
         try{
+            ArrayList<String> array = new ArrayList();
+            array.add("shehani.9716@gmail.com");
+            array.add("lasitheranga1@gmail.com");
+            array.add("sanjuhashintha@gmail.com");
             message.setFrom("lms2022.spring@gmail.com");
-            message.setTo(toEmail);
+
+
+            String[] ar = array.toArray(new String[array.size()]);  //2
+
+            message.setTo(ar);
             message.setText(body);
             message.setSubject(subject);
 
             mailSender.send(message);
             System.out.println("mail send...");
-
         }catch (Exception e){
             System.out.println("error" + e.getMessage());
         }
@@ -55,4 +65,3 @@ public class EmailClientService {
         }
     }
 }
-
